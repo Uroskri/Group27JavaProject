58042a3a239b7efe81fa1b75f26c23c19b4a3812
KMN <kuwwatt@gmail.com>
2022-12-14 14:03:32 +0100

    12/14/2022

---
+++ src/GitNotes.java
@@ -0,0 +1,26 @@
+public class GitNotes {
+
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                "\"I finished my job you can update your project\"\n" +
+                "for to see that comment we have to go Git  module and click Fetch\n" +
+                "\n" +
+                "for copy of code click Git below(alttaki) and click Local>master==> new branch from master\n" +
+                "//when we create new branch we have gave them name and for that we use date and project name or ticket name-US\n" +
+                "//master means main project\n" +
+                "\n" +
+                "//if you work in company PLEASE CREATE YOUR OWN BRANCH THEN START WORK\n" +
+                "//WE ALWAYS WORK COPY MAIN PROJECT, we are not working inside the master\n" +
+                "//FOR EACH JOB WE CREATE COPY\n" +
+                "//MASTER SHOULD BE PROTECTED\n" +
+                "//WHEN WE FINISHED WE ARE MERGING OUR CODES TOGETHER\n" +
+                "//BECAUSE WE WANT TO SEE OUR CODES FROM OTHERS EYES\n" +
+                "\n" +
+                "\n" +
+                "//we created copy of master and we wrote. if codes that we wrote and unliked and we want to delete it\n" +
+                "//for that we have to go master==>checkout==>then click copy and delete\n" +
+                "\n" +
+                "\n" +
+                "//what happened wrong copy of IDE? go to the commit and click your wrong class and rollback\n");
+    }
+}
